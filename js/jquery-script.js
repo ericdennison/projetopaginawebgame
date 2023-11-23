f374400550769f004757fe76415549fa0cc7ea13
eddesks <ericdenniso@gmail.com>
2023-11-23 11:43:18 -0300

    modificado:jquery-script.js, adicionado botao voltar - login.html

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -4,10 +4,19 @@ $(document).ready(function(){
 
     $('#botao-cadastrar').click(function(){
 
+        $('#botao-voltar').show();
         $('#form-cadastrar').slideToggle('slow');
         $('#section-login').slideToggle('slow');
         $('#botao-cadastrar').hide();
 
       
     });
+
+    $('#botao-voltar').click(function(){
+
+        $('#botao-cadastrar').show();
+        $('#form-cadastrar').slideToggle('slow');
+        $('#section-login').slideToggle('slow');
+        $('#botao-voltar').hide();
+    })
 });
